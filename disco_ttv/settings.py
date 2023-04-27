7e3821c77339ff37f8b7814a0d6bcbb49f79c2ff
Goyder <joshua@goyder.net.au>
2023-04-27 20:43:26 +0800

    Removing DL dependencies

--- disco_ttv/settings.py
+++ disco_ttv/settings.py
@@ -10,20 +10,9 @@ class Settings(BaseSettings):
     audio_clip_directory: str
 
     # Items associated with model training
-    model_directory: str
     training_datasets_directory: str
     output_directory: str
     test_dataset_name: str
-    root_tacotron2_model_name: Optional(str)
-    root_waveglow_model_name: Optional(str)
-
-    @property
-    def root_tacotron2_model_filepath(self):
-        return os.path.join(self.model_directory, self.root_tacotron2_model_name)
-
-    @property
-    def root_waveglow_model_filepath(self):
-        return os.path.join(self.model_directory, self.root_tacotron2_model_name)
 
     class Config:
         env_prefix = "DISCO_"
